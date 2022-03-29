cab02ff5771de63ea8572c0e21b45b76ccef6d1f
Satyam Sheth <samsheth92@gmail.com>
2022-03-28 21:53:47 -0400

    init3

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const alert = require('./index')
 
-alert({
-	type: "success"
-})
+alert({})
+alert({type: "info", msg: 'hekljlkjllo' })
+alert({type: "error", msg: 'helkjkljlo' })
+alert({type: "success", msg: 'heljkjkllo', name: 'DONE' })
+alert({type: "warning", msg: 'helkjkjkkjkjklo' })
